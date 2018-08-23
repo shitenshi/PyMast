cd75bbcf557dc66448931422e454a6ab0d8c04d7
shitenshi <yuuki.1226.saito@gmail.com>
2018-08-24 08:27:50 +0900

    first commit

---
+++ Model/mastodonapi.py
@@ -0,0 +1,36 @@
+from mastodon import Mastodon, StreamListener
+import yaml
+
+Mastodon.create_app(
+        'otome',
+        api_base_url = 'https://mstdn.maud.io',
+        to_file='c_token.secret'
+        )
+with open('p.yaml') as passfile:
+    info = yaml.load(passfile)
+mstdn = Mastodon(
+        client_id = 'c_token.secret',
+        api_base_url = 'https://mstdn.maud.io')
+mstdn.log_in(username = info['address'],
+        password = info['password'],
+        scopes=['read', 'write'],
+        to_file ='access_token.secret'
+        )
+mstdn = Mastodon(
+        access_token = 'access_token.secret',
+        api_base_url = 'https://mstdn.maud.io'
+        )
+
+class MstdnStreamListener(StreamListener):
+    def on_update(self, status):
+        print(status['content'])
+    def on_notification(self, notification):
+        print("")
+    def on_delete(self, status_id):
+        print("")
+        
+    def handle_heartbeat(self):
+        print("")
+mstdn.stream_user(MstdnStreamListener())
+
+
